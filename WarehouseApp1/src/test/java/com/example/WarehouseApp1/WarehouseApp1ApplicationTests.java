bdc6fc8d5a61a70284f9f30975cf19afd4f3698b
Manjunadh Parlapally <2200031972@kluniversity.in>
2025-03-06 10:43:01 +0530

    Add files via upload

---
+++ WarehouseApp1/src/test/java/com/example/WarehouseApp1/WarehouseApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WarehouseApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
